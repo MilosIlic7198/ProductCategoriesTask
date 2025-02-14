e3adccae1d7f50ea0b3da950ac523d92975c2be8
MilosIlic7198 <m.iWork71@gmail.com>
2025-02-14 20:58:50 +0100

    Added database migrations!

---
+++ database/migrations/2025_02_14_192559_create_products_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_number')->unique();
+            $table->foreignId('category_id')->constrained()->onDelete('set null');
+            $table->foreignId('department_id')->constrained()->onDelete('set null');
+            $table->foreignId('manufacturer_id')->constrained()->onDelete('set null');
+            $table->string('upc');
+            $table->string('sku');
+            $table->decimal('regular_price', 10, 2);
+            $table->decimal('sale_price', 10, 2);
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes();  //Added soft delete column.
+
+            //Adding indexes to the foreign key columns.
+            $table->index('category_id');
+            $table->index('department_id');
+            $table->index('manufacturer_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
