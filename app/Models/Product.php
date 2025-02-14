fbf831f83eddcbb8b3303ee079127001c208ebde
MilosIlic7198 <m.iWork71@gmail.com>
2025-02-14 21:22:55 +0100

    Added command and models!

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+
+    protected $fillable = [
+        'product_number', 'category_id', 'manufacturer_id', 'department_id', 'upc', 'sku', 'regular_price', 'sale_price', 'description',
+    ];
+}
